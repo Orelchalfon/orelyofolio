f061e9d15c9347a4e8c2aecdf8ef991b2e3e14a8
Orel Chalfon <103241272+Orelchalfon@users.noreply.github.com>
2024-10-14 04:31:16 +0300

    Configure tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 function App() {
-    return <div className="h-screen w-full bg-gray-200 text-2xl">kuh</div>;
+  return <div className="h-screen w-full bg-gray-200 text-2xl">kuh
+  aasd
+  </div>;
 }
 
 export default App;
